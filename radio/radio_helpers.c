f4c962f34f34148b77b8fb363d2241c7ef5017f0
Reto Da Forno <reto.daforno@tik.ee.ethz.ch>
2020-07-10 11:22:24 +0200

    typo fixed

--- radio/radio_helpers.c
+++ radio/radio_helpers.c
@@ -269,7 +269,7 @@ void radio_set_config_rxtx(uint8_t modulation_index,
   uint32_t fdev = 0;
   int32_t  bandwidth_rx = 0;
   uint32_t freq = radio_bands[current_band].centerFrequency;
-  if (modulation == 1) {
+  if (modulation_index == 1) {
     uint32_t afc_bandwidth = 2 * (bandwidth / 2 + freq) / RADIO_CLOCK_DRIFT;
     fdev = (bandwidth - datarate) / 2;
     bandwidth_rx = bandwidth + afc_bandwidth;
