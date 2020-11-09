bbcc1603834f82f63ce4c95c7839ac90896912d9
Reto Da Forno <reto.daforno@tik.ee.ethz.ch>
2020-11-09 14:17:23 +0100

    function added

--- radio/radio_helpers.c
+++ radio/radio_helpers.c
@@ -548,6 +548,17 @@ uint32_t radio_get_last_pkt_snr(void)
 }
 
 
+uint32_t radio_get_last_pkt_rssi(void)
+{
+  PacketStatus_t pktStatus;
+  SX126xGetPacketStatus(&pktStatus);
+  if (pktStatus.packetType == PACKET_TYPE_GFSK) {
+    return pktStatus.Params.Gfsk.RssiSync;  // or: .RssiAvg
+  }
+  return pktStatus.Params.LoRa.RssiPkt;
+}
+
+
 int32_t radio_get_rssi(void)
 {
   return SX126xGetRssiInst();   // instantaneous RSSI value -> radio needs to be in RX mode for this
