46a0cfc9f8f0706799e853988d3e11ae961ce3b1
Reto Da Forno <reto.daforno@tik.ee.ethz.ch>
2021-02-04 11:38:16 +0100

    formatting and comments added about the max. radio duty cycle

--- radio/radio_constants.c
+++ radio/radio_constants.c
@@ -6,162 +6,166 @@
 const radio_config_t radio_modulations[RADIO_NUM_MODULATIONS] =
 {
     // NOTE: bandwidth is the DSB
-        {   // 0
-            .modem = MODEM_LORA,
-            .bandwidth = 0,
-            .datarate = 12,
-            .coderate = 1,
-            .preambleLen = 10,
-        },
-        {   // 1
-            .modem = MODEM_LORA,
-            .bandwidth = 0,
-            .datarate = 11,
-            .coderate = 1,
-            .preambleLen = 10,
-        },
-        {   // 2
-            .modem = MODEM_LORA,
-            .bandwidth = 0,
-            .datarate = 10,
-            .coderate = 1,
-            .preambleLen = 10,
-        },
-        {   // 3
-            .modem = MODEM_LORA,
-            .bandwidth = 0,
-            .datarate = 9,
-            .coderate = 1,
-            .preambleLen = 10,
-        },
-        {   // 4
-            .modem = MODEM_LORA,
-            .bandwidth = 0,
-            .datarate = 8,
-            .coderate = 1,
-            .preambleLen = 10,
-        },
-        {   // 5
-            .modem = MODEM_LORA,
-            .bandwidth = 0,
-            .datarate = 7,
-            .coderate = 1,
-            .preambleLen = 10,
-        },
-        {   // 6
-            .modem = MODEM_LORA,
-            .bandwidth = 0,
-            .datarate = 6,
-            .coderate = 1,
-            .preambleLen = 12,
-        },
-        {   // 7
-            .modem = MODEM_LORA,
-            .bandwidth = 0,
-            .datarate = 5,
-            .coderate = 1,
-            .preambleLen = 12,
-        },
-        {   // 8
-            .modem = MODEM_FSK,
-            .bandwidth = 234300,
-            .datarate = 125000,
-            .fdev = 50000,
-            .preambleLen = 2,
-        },
-        {   // 9
-            .modem = MODEM_FSK,
-            .bandwidth = 234300,
-            .datarate = 200000,
-            .fdev = 10000,
-            .preambleLen = 2,
-        },
-        {   // 10
-            .modem = MODEM_FSK,
-            .bandwidth = 312000,
-            .datarate = 250000,
-            .fdev = 23500,
-            .preambleLen = 4,
-        },
+    {   // 0
+        .modem = MODEM_LORA,
+        .bandwidth = 0,
+        .datarate = 12,
+        .coderate = 1,
+        .preambleLen = 10,
+    },
+    {   // 1
+        .modem = MODEM_LORA,
+        .bandwidth = 0,
+        .datarate = 11,
+        .coderate = 1,
+        .preambleLen = 10,
+    },
+    {   // 2
+        .modem = MODEM_LORA,
+        .bandwidth = 0,
+        .datarate = 10,
+        .coderate = 1,
+        .preambleLen = 10,
+    },
+    {   // 3
+        .modem = MODEM_LORA,
+        .bandwidth = 0,
+        .datarate = 9,
+        .coderate = 1,
+        .preambleLen = 10,
+    },
+    {   // 4
+        .modem = MODEM_LORA,
+        .bandwidth = 0,
+        .datarate = 8,
+        .coderate = 1,
+        .preambleLen = 10,
+    },
+    {   // 5
+        .modem = MODEM_LORA,
+        .bandwidth = 0,
+        .datarate = 7,
+        .coderate = 1,
+        .preambleLen = 10,
+    },
+    {   // 6
+        .modem = MODEM_LORA,
+        .bandwidth = 0,
+        .datarate = 6,
+        .coderate = 1,
+        .preambleLen = 12,
+    },
+    {   // 7
+        .modem = MODEM_LORA,
+        .bandwidth = 0,
+        .datarate = 5,
+        .coderate = 1,
+        .preambleLen = 12,
+    },
+    {   // 8
+        .modem = MODEM_FSK,
+        .bandwidth = 234300,
+        .datarate = 125000,
+        .fdev = 50000,
+        .preambleLen = 2,
+    },
+    {   // 9
+        .modem = MODEM_FSK,
+        .bandwidth = 234300,
+        .datarate = 200000,
+        .fdev = 10000,
+        .preambleLen = 2,
+    },
+    {   // 10
+        .modem = MODEM_FSK,
+        .bandwidth = 312000,
+        .datarate = 250000,
+        .fdev = 23500,
+        .preambleLen = 4,
+    },
 };
 
 #ifndef US915
 const radio_band_t radio_bands[RADIO_NUM_BANDS] =
 {
-        { .centerFrequency = 863062500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 0
-        { .centerFrequency = 863187500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 1
-        { .centerFrequency = 863312500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 2
-        { .centerFrequency = 863437500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 3
-        { .centerFrequency = 863562500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 4
-        { .centerFrequency = 863687500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 5
-        { .centerFrequency = 863812500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 6
-        { .centerFrequency = 863937500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 7
-        { .centerFrequency = 864062500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 8
-        { .centerFrequency = 864187500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 9
-        { .centerFrequency = 864312500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 10
-        { .centerFrequency = 864437500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 11
-        { .centerFrequency = 864562500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 12
-        { .centerFrequency = 864687500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 13
-        { .centerFrequency = 864812500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 14
-        { .centerFrequency = 864937500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 15
-        { .centerFrequency = 865062500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 16
-        { .centerFrequency = 865187500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 17
-        { .centerFrequency = 865312500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 18
-        { .centerFrequency = 865437500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 19
-        { .centerFrequency = 865562500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 20
-        { .centerFrequency = 865687500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 21
-        { .centerFrequency = 865812500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 22
-        { .centerFrequency = 865937500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 23
-        { .centerFrequency = 866062500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 24
-        { .centerFrequency = 866187500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 25
-        { .centerFrequency = 866312500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 26
-        { .centerFrequency = 866437500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 27
-        { .centerFrequency = 866562500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 28
-        { .centerFrequency = 866687500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 29
-        { .centerFrequency = 866812500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 30
-        { .centerFrequency = 866937500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 31
-        { .centerFrequency = 867062500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 32
-        { .centerFrequency = 867187500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 33
-        { .centerFrequency = 867312500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 34
-        { .centerFrequency = 867437500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 35
-        { .centerFrequency = 867562500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 36
-        { .centerFrequency = 867687500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 37
-        { .centerFrequency = 867812500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 38
-        { .centerFrequency = 867937500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 39
-        { .centerFrequency = 868062500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 40
-        { .centerFrequency = 868187500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 41
-        { .centerFrequency = 868312500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 42
-        { .centerFrequency = 868437500.0, .bandwidth = 125000.0, .dutyCycle = 10, .maxPower = 14 }, // 43
-        { .centerFrequency = 868762500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 44
-        { .centerFrequency = 868887500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 45
-        { .centerFrequency = 869012500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 46
-        { .centerFrequency = 869137500.0, .bandwidth = 125000.0, .dutyCycle = 1, .maxPower = 14 }, // 47
-        { .centerFrequency = 869462500.0, .bandwidth = 125000.0, .dutyCycle = 100, .maxPower = 26 }, // 48
-        { .centerFrequency = 869587500.0, .bandwidth = 125000.0, .dutyCycle = 100, .maxPower = 26 }, // 49
-        { .centerFrequency = 869762500.0, .bandwidth = 125000.0, .dutyCycle = 100, .maxPower = 7 }, // 50
-        { .centerFrequency = 869887500.0, .bandwidth = 125000.0, .dutyCycle = 100, .maxPower = 7 }, // 51
+    /* Notes:
+     * - duty cycle is in 0.1% steps (e.g. 100 = 10%)
+     * - general rule for non-specific SRD without any collision avoidance for the 863 - 870 MHz band: max. 0.1% duty cycle at 14dBm (25mW)
+     */
+    { .centerFrequency = 863062500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 0
+    { .centerFrequency = 863187500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 1
+    { .centerFrequency = 863312500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 2
+    { .centerFrequency = 863437500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 3
+    { .centerFrequency = 863562500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 4
+    { .centerFrequency = 863687500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 5
+    { .centerFrequency = 863812500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 6
+    { .centerFrequency = 863937500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 7
+    { .centerFrequency = 864062500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 8
+    { .centerFrequency = 864187500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 9
+    { .centerFrequency = 864312500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 10
+    { .centerFrequency = 864437500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 11
+    { .centerFrequency = 864562500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 12
+    { .centerFrequency = 864687500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 13
+    { .centerFrequency = 864812500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 14
+    { .centerFrequency = 864937500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 15
+    { .centerFrequency = 865062500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 16
+    { .centerFrequency = 865187500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 17
+    { .centerFrequency = 865312500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 18
+    { .centerFrequency = 865437500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 19
+    { .centerFrequency = 865562500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 20
+    { .centerFrequency = 865687500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 21
+    { .centerFrequency = 865812500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 22
+    { .centerFrequency = 865937500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 23
+    { .centerFrequency = 866062500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 24
+    { .centerFrequency = 866187500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 25
+    { .centerFrequency = 866312500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 26
+    { .centerFrequency = 866437500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 27
+    { .centerFrequency = 866562500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 28
+    { .centerFrequency = 866687500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 29
+    { .centerFrequency = 866812500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 30
+    { .centerFrequency = 866937500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 31
+    { .centerFrequency = 867062500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 32
+    { .centerFrequency = 867187500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 33
+    { .centerFrequency = 867312500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 34
+    { .centerFrequency = 867437500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 35
+    { .centerFrequency = 867562500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 36
+    { .centerFrequency = 867687500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 37
+    { .centerFrequency = 867812500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 38
+    { .centerFrequency = 867937500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 39
+    { .centerFrequency = 868062500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 40
+    { .centerFrequency = 868187500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 41
+    { .centerFrequency = 868312500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 42
+    { .centerFrequency = 868437500, .bandwidth = 125000, .dutyCycle = 10, .maxPower = 14 }, // 43
+    { .centerFrequency = 868762500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 44
+    { .centerFrequency = 868887500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 45
+    { .centerFrequency = 869012500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 46
+    { .centerFrequency = 869137500, .bandwidth = 125000, .dutyCycle = 1, .maxPower = 14 }, // 47
+    { .centerFrequency = 869462500, .bandwidth = 125000, .dutyCycle = 100, .maxPower = 26 }, // 48
+    { .centerFrequency = 869587500, .bandwidth = 125000, .dutyCycle = 100, .maxPower = 26 }, // 49
+    { .centerFrequency = 869762500, .bandwidth = 125000, .dutyCycle = 100, .maxPower = 7 }, // 50       up to 100% duty cycle @7dBm or 1% @14dBm
+    { .centerFrequency = 869887500, .bandwidth = 125000, .dutyCycle = 100, .maxPower = 7 }, // 51       up to 100% duty cycle @7dBm or 1% @14dBm
 };
 
 const radio_band_group_t lora_band_groups[] = {
-        { .lower = 0, .upper = 15 },
-        { .lower = 16, .upper = 39 },
-        { .lower = 40, .upper = 43 },
-        { .lower = 44, .upper = 47 },
-        { .lower = 48, .upper = 49 },
-        { .lower = 50, .upper = 51 },
+    { .lower = 0, .upper = 15 },
+    { .lower = 16, .upper = 39 },
+    { .lower = 40, .upper = 43 },
+    { .lower = 44, .upper = 47 },
+    { .lower = 48, .upper = 49 },
+    { .lower = 50, .upper = 51 },
 };
 
 #else
 const lora_band_t radio_bands[] =
 {
-        { .centerFrequency = 914812500.0, .bandwidth = 125000.0, .dutyCycle = 100, .maxPower = 26 }, // 0
-        { .centerFrequency = 914937500.0, .bandwidth = 125000.0, .dutyCycle = 100, .maxPower = 26 }, // 1
-        { .centerFrequency = 915062500.0, .bandwidth = 125000.0, .dutyCycle = 100, .maxPower = 26 }, // 2
-        { .centerFrequency = 915187500.0, .bandwidth = 125000.0, .dutyCycle = 100, .maxPower = 26 }, // 3
+    { .centerFrequency = 914812500, .bandwidth = 125000, .dutyCycle = 100, .maxPower = 26 }, // 0
+    { .centerFrequency = 914937500, .bandwidth = 125000, .dutyCycle = 100, .maxPower = 26 }, // 1
+    { .centerFrequency = 915062500, .bandwidth = 125000, .dutyCycle = 100, .maxPower = 26 }, // 2
+    { .centerFrequency = 915187500, .bandwidth = 125000, .dutyCycle = 100, .maxPower = 26 }, // 3
 };
 
 const radio_band_group_t radio_band_groups[] = {
-        { .lower = 0, .upper = 3 },
+    { .lower = 0, .upper = 3 },
 };
 
 #endif
